e911a8623b195393358cf337da7f58a34f9f5069
ABK Bhuiyan <jehadfeni@gmail.com>
2021-09-04 01:27:47 +0600

    VPN Added to all views

--- app/admin/AddWebsiteLink.php
+++ app/admin/AddWebsiteLink.php
@@ -9,4 +9,7 @@ class AddWebsiteLink extends Model
     public function shortLinks(){
         return $this->hasMany(ShortLink::class,'website_link_id');
     }
+    public function shortLinkClicks(){
+        return $this->hasMany(ShortlinkClick::class,'short_link_id');
+    }
 }
